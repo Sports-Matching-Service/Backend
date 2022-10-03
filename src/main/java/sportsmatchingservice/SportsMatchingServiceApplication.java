ef32945d5e0fc8332e248abd11c139a5e1588711
yeonkkk <tjddus0302@gmail.com>
2022-10-03 23:49:44 +0900

    Feat: 프로젝트 생성

---
+++ src/main/java/sportsmatchingservice/SportsMatchingServiceApplication.java
@@ -0,0 +1,12 @@
+package sportsmatchingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsMatchingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsMatchingServiceApplication.class, args);
+	}
+}
