9f0c4cc7f2988d036ef5d80d273ddb64f3c2dc55
yeonkkk <tjddus0302@gmail.com>
2022-10-09 01:47:28 +0900

    Refactor: 비밀번호 인코딩 적용

--- src/main/java/sportsmatchingservice/users/service/UserService.java
+++ src/main/java/sportsmatchingservice/users/service/UserService.java
@@ -1,23 +1,32 @@
 package sportsmatchingservice.users.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import sportsmatchingservice.constant.ErrorCode;
 import sportsmatchingservice.exceptions.exception.DuplicatedDataException;
 import sportsmatchingservice.exceptions.exception.GeneralException;
+import sportsmatchingservice.users.domain.User;
 import sportsmatchingservice.users.dto.UserPostDto;
 import sportsmatchingservice.users.repository.UserRepository;
 
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public boolean createUser(UserPostDto userPostDto) {
         try {
             if (userPostDto == null) return false;
-            userRepository.save(userPostDto.toEntity());
+
+            User user = userPostDto.toEntity();
+            user.setPassword(passwordEncoder.encode(userPostDto.getPassword()));
+
+            userRepository.save(user);
             return true;
         } catch (Exception e) {
             throw new DuplicatedDataException(ErrorCode.BAD_REQUEST, e);
