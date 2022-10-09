70ae3fd5a3ef7ffcd5f09ac32c33a96b9e85cb4a
yeonkkk <tjddus0302@gmail.com>
2022-10-09 15:57:26 +0900

    Refactor: 회원 권한 설정
    - User: roles 추가
    - UserRepository: email로 User를 찾기 위해 findByEmail 추가
    - UserService: createUser에 권한 부여 코드 추가
    - 이 외 불필요한 import 제거

--- src/main/java/sportsmatchingservice/users/repository/UserRepository.java
+++ src/main/java/sportsmatchingservice/users/repository/UserRepository.java
@@ -3,6 +3,8 @@ package sportsmatchingservice.users.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sportsmatchingservice.users.domain.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
--- src/main/java/sportsmatchingservice/users/service/UserService.java
+++ src/main/java/sportsmatchingservice/users/service/UserService.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import sportsmatchingservice.auth.utils.CustomAuthorityUtils;
 import sportsmatchingservice.constant.ErrorCode;
 import sportsmatchingservice.exceptions.exception.DuplicatedDataException;
-import sportsmatchingservice.exceptions.exception.GeneralException;
 import sportsmatchingservice.users.domain.User;
 import sportsmatchingservice.users.dto.UserPostDto;
 import sportsmatchingservice.users.repository.UserRepository;
+
+import java.util.List;
 
 @Transactional
 @RequiredArgsConstructor
@@ -19,12 +21,18 @@ public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
+    private final CustomAuthorityUtils authorityUtils;
+
+
     public boolean createUser(UserPostDto userPostDto) {
         try {
             if (userPostDto == null) return false;
 
             User user = userPostDto.toEntity();
             user.setPassword(passwordEncoder.encode(userPostDto.getPassword()));
+
+            List<String> roles = authorityUtils.createRoles(user.getEmail());
+            user.setRoles(roles);
 
             userRepository.save(user);
             return true;
