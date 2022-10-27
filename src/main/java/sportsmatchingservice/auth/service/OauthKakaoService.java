bfd97a0bc03d61cea2c4bb94802dfc5b7a246708
jay <jaewhan232@naver.com>
2022-10-27 10:19:12 +0900

    Feat: getAccessToken() 메소드 구현 인증코드를 사용하여 kakao access token 발급

--- src/main/java/sportsmatchingservice/auth/service/OauthKakaoService.java
+++ src/main/java/sportsmatchingservice/auth/service/OauthKakaoService.java
@@ -1,16 +1,32 @@
 package sportsmatchingservice.auth.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import sportsmatchingservice.auth.dto.OauthTokenDto;
 
 import java.util.*;
 
 @Service
 @RequiredArgsConstructor
 public class OauthKakaoService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Getter
     @Value("${social.kakao.params.clientId}")
@@ -40,4 +56,26 @@ public class OauthKakaoService {
         return parameters;
     }
 
+    public String getAccessToken(String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.set("grant_type", "authorization_code");
+        params.set("client_id", getClientId());
+        params.set("redirect_uri", getRedirectUri());
+        params.set("code", code);
+        params.set("client_secret", getClientSecret());
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = restTemplate
+                .postForEntity(getTokenUrl(), request, String.class);
+
+        try {
+            return objectMapper.readValue(response.getBody(), OauthTokenDto.class).getAccessToken();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
