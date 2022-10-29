a204b50293915cdc88364a89ce580cf6fdf5f546
yeonkkk <tjddus0302@gmail.com>
2022-10-29 17:27:49 +0900

    Feat: UserController 수정
    - 중복코드 삭제: @RequestMapping
    - getOauthParams, getUserTokenDto에 네이버 로그인에 대한 처리 추가

--- src/main/java/sportsmatchingservice/auth/controller/UserController.java
+++ src/main/java/sportsmatchingservice/auth/controller/UserController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.*;
 import sportsmatchingservice.auth.dto.UserSignupDto;
 import sportsmatchingservice.auth.dto.UserTokenDto;
 import sportsmatchingservice.auth.service.OauthKakaoService;
+import sportsmatchingservice.auth.service.OauthNaverService;
 import sportsmatchingservice.constant.ErrorCode;
 import sportsmatchingservice.constant.dto.ApiDataResponse;
 import sportsmatchingservice.auth.service.UserService;
 
-@RequestMapping("/users")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/users")
@@ -18,6 +18,7 @@ public class UserController {
 
     private final UserService userService;
     private final OauthKakaoService oauthKakaoService;
+    private final OauthNaverService oauthNaverService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
@@ -34,15 +35,22 @@ public class UserController {
     public ApiDataResponse getOauthParams(@PathVariable("social") String social) {
         if (social.equals("kakao")){
             return ApiDataResponse.of(ErrorCode.OK, oauthKakaoService.getParameters());
+        } else if (social.equals("naver")) {
+            return ApiDataResponse.of(ErrorCode.OK, oauthNaverService.getParameters());
         } else {
             return ApiDataResponse.of(ErrorCode.INTERNAL_ERROR, null);
         }
     }
 
     @RequestMapping("/oauth/tokens/{social}")
-    public ApiDataResponse getUserTokenDto(@PathVariable("social") String social,String code) {
+    public ApiDataResponse getUserTokenDto(@PathVariable("social") String social,
+                                           @RequestParam String code,
+                                           @RequestParam(required = false) String state) {
         if (social.equals("kakao")) {
             UserTokenDto userTokenDto = oauthKakaoService.getUserToken(code);
+            return ApiDataResponse.of(ErrorCode.OK, userTokenDto);
+        } else if (social.equals("naver")) {
+            UserTokenDto userTokenDto = oauthNaverService.getUserToken(code, state);
             return ApiDataResponse.of(ErrorCode.OK, userTokenDto);
         } else {
             return ApiDataResponse.of(ErrorCode.INTERNAL_ERROR, null);
