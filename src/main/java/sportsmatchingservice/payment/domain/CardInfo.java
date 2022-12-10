6ef0245e9240347e075acc82d1e4b2874f1d8bce
yeonkkk <tjddus0302@gmail.com>
2022-12-10 15:16:24 +0900

    Refactor: Amount, CardInfo 테이블 수정

--- src/main/java/sportsmatchingservice/payment/domain/CardInfo.java
+++ src/main/java/sportsmatchingservice/payment/domain/CardInfo.java
@@ -1,19 +1,43 @@
 package sportsmatchingservice.payment.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class CardInfo {
+    @Id
+    @GeneratedValue
+    private int id;
+    @Column
     private String purchaseCorp;
+    @Column
     private String purchaseCorpCode;
+    @Column
     private String issuerCorp;
+    @Column
     private String issuerCorpCode;
+    @Column
     private String kakaopayPurchaseCorp;
+    @Column
     private String kakaopayPurchaseCorpCode;
+    @Column
     private String kakaopayIssuerCorp;
+    @Column
     private String kakaopayIssuerCorpCode;
+    @Column
     private String bin;
+    @Column
     private String cardType;
+    @Column
     private String installMonth;
+    @Column
     private String approvedId;
+    @Column
     private String cardMin;
+    @Column
     private String interestFreeInstall;
+    @Column
     private String cardItemCode;
 }
